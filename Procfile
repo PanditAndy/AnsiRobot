d7f1c27cfd5d10e38c44f90b498f61d4641c61b2
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2023-04-21 07:20:09 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m AnsiRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE AnsiRobot
+:: Enables virtual env mode and then starts Ansi
+env\scripts\activate.bat && py -m AnsiRobot
